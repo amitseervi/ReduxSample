2563ab9ebbb51e390a14ae6660d03e77b64be581
Amit <amitchaudhary.adc@gmail.com>
2020-08-25 11:44:43 +0530

    Changing button styles

--- app/components/CounterComponent.tsx
+++ app/components/CounterComponent.tsx
@@ -1,5 +1,5 @@
 import React, {Dispatch} from 'react';
-import {Text, Button, View} from 'react-native';
+import {Text, Button, View, StyleSheet, TouchableOpacity} from 'react-native';
 import {
   increamentCounter,
   decrementCounter,
@@ -21,13 +21,64 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 const CounterComponent = (props: PropsFromRedux) => {
+  console.log('Hello world');
+  console.dir(props);
   return (
-    <View>
-      <Text>{props.counter}</Text>
-      <Button title="Add" onPress={props.onInc} />
-      <Button title="Subtract" onPress={props.onDec} />
+    <View style={styles.container}>
+      <Text style={styles.counterText}>{props.counter}</Text>
+      <View style={styles.counterButtonContainer}>
+        <TouchableOpacity
+          style={[styles.counterActionContainer, styles.plusActionContainer]}
+          onPress={props.onInc}>
+          {/* <Button title="Add" onPress={props.onInc} /> */}
+          <Text>Add</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.counterActionContainer, styles.minusActionContainer]}
+          onPress={props.onDec}>
+          {/* <Button title="Subtract" onPress={props.onDec} /> */}
+          <Text>Subtract</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  counterText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  counterButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  counterActionContainer: {
+    padding: 10,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    margin: 10,
+  },
+  plusActionContainer: {
+    backgroundColor: '#5999ff',
+  },
+  minusActionContainer: {
+    backgroundColor: '#ff6c59',
+  },
+});
 
 export default connector(CounterComponent);
